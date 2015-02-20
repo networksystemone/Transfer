3b99082d4c3c03c3c1cf3fe2c9b1a04301ef1676
Kuba <jakub@c-call.eu>
2015-02-20 12:02:58 +0100

    promotions fix

--- src/Keios/TransferToTools/API/ApiCommands/CheckOperatorPromotionsApiCommand.php
+++ src/Keios/TransferToTools/API/ApiCommands/CheckOperatorPromotionsApiCommand.php
@@ -14,8 +14,7 @@ class CheckOperatorPromotionsApiCommand extends ApiCommand implements ApiCommand
         $login = $this->apiKey->getLogin();
         $key = $this->apiKey->getKey();
         $hash = $this->apiKey->getHash();
-        $destinationMsisdn = $this->arguments['destination'];
-        $operatorID = '';
+        $operatorID = $this->arguments['operatorid'];
         //$destination_msisdn = '+48663521642';  // if checking with phone number
         //$operator_id = 1;                      // if checking with operator id
 
@@ -25,7 +24,7 @@ class CheckOperatorPromotionsApiCommand extends ApiCommand implements ApiCommand
             <key>$key</key>
             <md5>$hash</md5>
             <action>msisdn_info</action>
-            <destination_msisdn>$destinationMsisdn</destination_msisdn>
+            <destination_msisdn></destination_msisdn>
             <delivered_amount_info>0</delivered_amount_info>
             <operatorid>$operatorID</operatorid>
             <return_promo>1</return_promo>
