de366357f7dc19251ad856c0a89a213fca21f948
Kuba <jakub@keios.eu>
2016-06-30 10:14:46 +0200

    fixes for guzzle 6

--- src/Keios/TransferToTools/API/RequestClients/GuzzleRequestClient.php
+++ src/Keios/TransferToTools/API/RequestClients/GuzzleRequestClient.php
@@ -3,63 +3,96 @@
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Psr7\Request;
 use Keios\TransferToTools\API\Contracts\RequestClientInterface;
 use Keios\TransferToTools\API\Contracts\TransferToApiKeyInterface;
 use Keios\TransferToTools\API\Exceptions\InvalidMethodException;
 use GuzzleHttp\Client;
 
+/**
+ * Class GuzzleRequestClient
+ * @package Keios\TransferToTools\API\RequestClients
+ */
 class GuzzleRequestClient implements RequestClientInterface
-{ 
+{
 
+    /**
+     *
+     */
     const BASE_URL = 'https://fm.transfer-to.com';
 
+    /**
+     *
+     */
     const PATH = 'cgi-bin/shop/topup';
 
+    /**
+     * @var Client
+     */
     protected $guzzleClient;
 
+    /**
+     * @var array
+     */
     protected $validMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
 
+    /**
+     * @var
+     */
     protected $lastStatusCode;
 
     /**
      * Create HTTP Request Client, Guzzle in this case
+     *
      * @param TransferToApiKeyInterface $apiKey
+     *
      * @throws \Keios\TransferToTools\API\Exceptions\NoCredentialsException
      * @return null
      */
     public function boot()
     {
-        $this->guzzleClient = new Client([
-            'base_url' => self::BASE_URL,
-            'defaults' => [
-                'headers' => [
-                    'Content-Type' => 'text/xml'
+        $this->guzzleClient = new Client(
+            [
+                'base_url' => self::BASE_URL,
+                'defaults' => [
+                    'headers' => [
+                        'Content-Type' => 'text/xml',
+                    ],
                 ],
             ]
-        ]);
+        );
     }
 
+    /**
+     * @param $method
+     * @param $body
+     *
+     * @return string
+     * @throws InvalidMethodException
+     */
     public function processRequest($method, $body)
     {
-        if (!in_array(strtoupper($method), $this->validMethods))
-            throw new InvalidMethodException('Method ' . $method . ' is not a valid method for this http client.');
+        if (!in_array(strtoupper($method), $this->validMethods)) {
+            throw new InvalidMethodException('Method '.$method.' is not a valid method for this http client.');
+        }
 
-        $request = $this->guzzleClient->createRequest($method, self::PATH, [
+        /* - for guzzle 3.9, to remove
+        $request = $this->guzzleClient->createRequest($method, self::BASE_URL.'/'.self::PATH, [
             'body' => $body,
             'stream' => false
         ]);
+         */
+        $request = new Request(
+            $method,
+            self::BASE_URL.'/'.self::PATH,
+            ['Content-Type' => 'text/xml; charset=UTF8'],
+            $body
+        );
 
         try {
-
-            /* TO REMOVE
-            echo 'Executing ' . $request->getUrl() . ' with request body: '; //todo
-            echo $request->getBody(); //todo
-            echo '<br /><br />';
-            /* END TO REMOVE */
-
             $response = $this->guzzleClient->send($request);
             $this->lastStatusCode = $response->getStatusCode();
-            return (string)$response->getBody();
+            return $response->getBody()->getContents();
 
         } catch (ClientException $e) {
             return $this->handleClientException($e);
@@ -70,16 +103,25 @@ class GuzzleRequestClient implements RequestClientInterface
         }
     }
 
+    /**
+     * @return mixed
+     */
     public function getLastStatusCode()
     {
         return $this->lastStatusCode;
     }
 
+    /**
+     * @param RequestException $exception
+     *
+     * @return string
+     */
     protected function handleRequestException(RequestException $exception)
     {
         if ($exception->hasResponse()) {
             $response = $exception->getResponse();
             $this->lastStatusCode = $response->getStatusCode();
+
             return (string)$response->getBody();
         } else {
             /*
@@ -89,11 +131,17 @@ class GuzzleRequestClient implements RequestClientInterface
         }
     }
 
+    /**
+     * @param ServerException $exception
+     *
+     * @return string
+     */
     protected function handleServerException(ServerException $exception)
     {
         if ($exception->hasResponse()) {
             $response = $exception->getResponse();
             $this->lastStatusCode = $response->getStatusCode();
+
             return (string)$response->getBody();
         } else {
             /*
@@ -103,11 +151,17 @@ class GuzzleRequestClient implements RequestClientInterface
         }
     }
 
+    /**
+     * @param ClientException $exception
+     *
+     * @return string
+     */
     protected function handleClientException(ClientException $exception)
     {
         if ($exception->hasResponse()) {
             $response = $exception->getResponse();
             $this->lastStatusCode = $response->getStatusCode();
+
             return (string)$response->getBody();
         } else {
             /*
